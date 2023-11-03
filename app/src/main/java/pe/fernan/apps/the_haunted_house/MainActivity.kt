962c11886622aec3be90eb862defbdae2645f767
FernanApps <dev.fernan.apps@gmail.com>
2023-11-03 10:59:28 -0500

    Add FlipCard

--- app/src/main/java/pe/fernan/apps/the_haunted_house/MainActivity.kt
+++ app/src/main/java/pe/fernan/apps/the_haunted_house/MainActivity.kt
@@ -5,12 +5,18 @@ import android.util.Log
 import android.view.KeyEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -18,20 +24,24 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
@@ -41,6 +51,7 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import pe.fernan.apps.the_haunted_house.ui.theme.TheHauntedHouseTheme
 import kotlin.random.Random
 
@@ -156,6 +167,9 @@ fun Rooms(modifier: Modifier) {
     val requester = remember { FocusRequester() }
 
     val items = (1..16).toList()
+
+    val flipRandom = items.random()
+
     var focusedIndex by remember { mutableStateOf(0) }
 
     LazyVerticalGrid(
@@ -211,6 +225,7 @@ fun Rooms(modifier: Modifier) {
             } else {
                 Color.White
             }
+
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -219,20 +234,203 @@ fun Rooms(modifier: Modifier) {
                 colors = CardDefaults.cardColors(containerColor = Color.White)
             ) {
                 //Text(text = it.toString())
-                if(icon != null){
-                    Icon(
-                        modifier = Modifier.fillMaxSize().padding(15.dp),
-                        painter = painterResource(id = icon),
-                        contentDescription = "icon",
-                        tint = LightGreen
-                    )
+                if (icon != null) {
+
+                    if (it == flipRandom) {
+                        var currentCardFace by remember { mutableStateOf(CardFace.Front) }
+                        var isFlipped by remember { mutableStateOf(false) }
+
+                        FlipCardImp(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1f)
+                                .padding(2.dp),
+                            currentCardFace = currentCardFace
+                        )
+
+
+                        LaunchedEffect(key1 = currentCardFace){
+                            if(!isFlipped){
+                                delay(500)
+                                currentCardFace = currentCardFace.next
+                                isFlipped = true
+
+                            }
+
+                        }
+
+
+
+                    } else {
+                        Icon(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(15.dp),
+                            painter = painterResource(id = icon),
+                            contentDescription = "icon",
+                            tint = LightGreen
+                        )
+                    }
+
+
+
+
                 }
             }
+
+
         }
     }
 }
 
 
+// https://fvilarino.medium.com/creating-a-rotating-card-in-jetpack-compose-ba94c7dd76fb
+
+enum class CardFace(val angle: Float) {
+    Front(0f) {
+        override val next: CardFace
+            get() = Back
+    },
+    Back(180f) {
+        override val next: CardFace
+            get() = Front
+    };
+
+    abstract val next: CardFace
+}
+
+enum class RotationAxis {
+    AxisX,
+    AxisY,
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FlipCard(
+    cardFace: CardFace,
+    onClick: (CardFace) -> Unit,
+    modifier: Modifier = Modifier,
+    axis: RotationAxis = RotationAxis.AxisY,
+    back: @Composable () -> Unit = {},
+    front: @Composable () -> Unit = {},
+) {
+    val rotation = animateFloatAsState(
+        targetValue = cardFace.angle,
+        animationSpec = tween(
+            durationMillis = 400,
+            easing = FastOutSlowInEasing,
+        )
+    )
+    Card(
+        onClick = { onClick(cardFace) },
+        modifier = modifier
+            .graphicsLayer {
+                if (axis == RotationAxis.AxisX) {
+                    rotationX = rotation.value
+                } else {
+                    rotationY = rotation.value
+                }
+                cameraDistance = 12f * density
+            },
+    ) {
+        if (rotation.value <= 90f) {
+            Box(
+                Modifier.fillMaxSize()
+            ) {
+                front()
+            }
+        } else {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .graphicsLayer {
+                        if (axis == RotationAxis.AxisX) {
+                            rotationX = 180f
+                        } else {
+                            rotationY = 180f
+                        }
+                    },
+            ) {
+                back()
+            }
+        }
+    }
+}
+
+@Composable
+fun FlipCardImp(modifier: Modifier, currentCardFace: CardFace) {
+    //var currentCardFace by remember { mutableStateOf(CardFace.Front) }
+
+    FlipCard(
+        cardFace = currentCardFace,
+        onClick = {
+            //currentCardFace = it.next
+        },
+        modifier = modifier,
+        front = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Blue),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Frente", color = Color.White)
+            }
+        },
+        back = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Parte posterior", color = Color.White)
+            }
+        })
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun FlippableCardPreview() {
+    TheHauntedHouseTheme {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+            var currentCardFace by remember { mutableStateOf(CardFace.Front) }
+
+            FlipCard(
+                cardFace = currentCardFace,
+                onClick = {
+                    currentCardFace = it.next
+                },
+                modifier = Modifier
+                    .size(200.dp),
+                front = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Blue),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Frente", color = Color.White)
+                    }
+                },
+                back = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Red),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Parte posterior", color = Color.White)
+                    }
+                })
+        }
+
+    }
+
+}
 
 
 @Preview(showBackground = true)
